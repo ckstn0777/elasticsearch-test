76841c5b100e06cdacb7667211f56b50462a89f2
ckstn0777 <ckstn0777@github.com>
2022-07-10 20:13:18 +0900

    spring-batch 프로젝트 다시 생성

---
+++ batch/src/main/java/com/ckstn0777/batch/BatchApplication.java
@@ -0,0 +1,13 @@
+package com.ckstn0777.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+
+}
---
+++ batch/src/test/java/com/ckstn0777/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ckstn0777.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
