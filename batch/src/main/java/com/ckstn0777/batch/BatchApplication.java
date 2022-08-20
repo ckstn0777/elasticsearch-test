55258c1dc45f4e471ca7bbff30a9586e4957dba2
ckstn0777 <ckstn0777@naver.com>
2022-08-20 15:16:46 +0900

    Create JpaPagingItemReader, Elasticsearch bulk Writer

--- batch/src/main/java/com/ckstn0777/batch/BatchApplication.java
+++ batch/src/main/java/com/ckstn0777/batch/BatchApplication.java
@@ -1,5 +1,11 @@
 package com.ckstn0777.batch;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +18,20 @@ public class BatchApplication {
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    ElasticsearchClient elasticsearchClient() {
+        // Create the low-level client
+        RestClient restClient = RestClient.builder(
+                new HttpHost("localhost", 9200)).build();
+
+        // Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        // And create the API client
+        return new ElasticsearchClient(transport);
     }
 
     public static void main(String[] args) {
--- batch/src/main/java/com/ckstn0777/batch/config/ElasticBulkJobConfiguration.java
+++ batch/src/main/java/com/ckstn0777/batch/config/ElasticBulkJobConfiguration.java
@@ -1,14 +1,21 @@
 package com.ckstn0777.batch.config;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.BulkRequest;
+import co.elastic.clients.elasticsearch.core.BulkResponse;
+import co.elastic.clients.elasticsearch.core.bulk.BulkResponseItem;
 import com.ckstn0777.batch.entity.Book;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,23 +37,52 @@ public class ElasticBulkJobConfiguration {
     }
 
     @Bean
-    public Step elasticBulkStep() {
+    @JobScope
+    public Step elasticBulkStep(JpaPagingItemReader<Book> jpaBookReader, ItemWriter<Book> elasticBulkWriter) {
         return stepBuilderFactory.get("elasticBulkStep")
-                .<Book, Book>chunk(10)
-                .reader(jpaBookReader())
-                .writer(elasticBulkWriter())
+                .<Book, Book>chunk(1000)
+                .reader(jpaBookReader)
+                .writer(elasticBulkWriter)
                 .build();
     }
 
     @Bean
-    public ItemReader<Book> jpaBookReader() {
-        // TODO : 구현 예정
-        return null;
+    @StepScope
+    public JpaPagingItemReader<Book> jpaBookReader() {
+        return new JpaPagingItemReaderBuilder<Book>()
+                .name("jpaPagingItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(1000)
+                .queryString("SELECT b FROM Book b")
+                .build();
     }
 
     @Bean
-    public ItemWriter<Book> elasticBulkWriter() {
-        // TODO : 구현 예정
-        return null;
+    @StepScope
+    public ItemWriter<Book> elasticBulkWriter(ElasticsearchClient elasticsearchClient) {
+        return books -> {
+            log.info("Writing books: {}", books);
+            BulkRequest.Builder br = new BulkRequest.Builder();
+
+            for (Book book : books) {
+                br.operations(op -> op
+                        .index(idx -> idx
+                                .index("public_book")
+                                .id("public_book_" + book.getId().toString())
+                                .document(book)
+                        )
+                );
+            }
+
+            BulkResponse result = elasticsearchClient.bulk(br.build());
+            if (result.errors()) {
+                log.error("Bulk had errors");
+                for (BulkResponseItem item: result.items()) {
+                    if (item.error() != null) {
+                        log.error(item.error().reason());
+                    }
+                }
+            }
+        };
     }
 }
